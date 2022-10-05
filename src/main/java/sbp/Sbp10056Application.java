61e6550354cdede77be1c433f8ed1402e716fd19
nayeong <olive_yeong@naver.com>
2022-10-05 17:33:35 +0900

    221005

---
+++ src/main/java/sbp/Sbp10056Application.java
@@ -0,0 +1,13 @@
+package sbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbp10056Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbp10056Application.class, args);
+	}
+
+}
---
+++ src/main/java/sbp/controllers/PurchaseController.java
@@ -0,0 +1,33 @@
+package sbp.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sbp.model.Purchase;
+import sbp.repositories.PurchaseRepository;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+	
+	private final PurchaseRepository purchaseRepository;
+	
+	public PurchaseController(PurchaseRepository purchaseRepository) {
+		this.purchaseRepository =purchaseRepository;
+	}
+	
+	@PostMapping
+	public void storePurchase(@RequestBody Purchase purchase) {
+		purchaseRepository.storePurchase(purchase);
+	}
+	
+	@GetMapping
+	public List<Purchase> findPurchases(){
+		return purchaseRepository.findAllPurchases();
+	}
+}
